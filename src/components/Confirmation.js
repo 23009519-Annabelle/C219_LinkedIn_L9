d28811cdde427dd6f05f7807273cc03d2e7889e6
23009519-Annabelle <23009519@myrp.edu.sg>
2025-01-13 22:31:11 +0800

    C219_L9_LinkedIn

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -6,7 +6,6 @@ export default function Confirmation() {
   return (
     <div className="container">
       <h1>Thank You!</h1>
-
       {state && (
         <>
           <p>{state.name}, you're now registered for Red30 Tech.</p>
